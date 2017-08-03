7428663aca6a297f406e3d1e50f2ddd2f97bd5e1
Justin Moss <Justin.Moss@gmail.com>
2017-08-03 12:02:07 -0400

    Add build script

---
+++ build/buildRockLibEncryptionNuget.bat
@@ -0,0 +1,7 @@
+nuget restore -SolutionDirectory ../  ../Rock.Encryption\RockLib.Encryption.csproj
+
+msbuild /p:Configuration=Release /t:Clean ..\Rock.Encryption\RockLib.Encryption.csproj
+
+msbuild /p:Configuration=Release /t:Rebuild ..\Rock.Encryption\RockLib.Encryption.csproj
+
+msbuild /t:pack /p:PackageOutputPath=..\builtPackages  /p:Configuration=Release ..\Rock.Encryption\RockLib.Encryption.csproj
