8f73fd799d15683ec5616a61b1f2ec22127d514e
Justin Moss <Justin.Moss@gmail.com>
2017-09-15 15:35:21 -0400

    Add DotNetCore and DotNetFramework examples

---
+++ Example.Encryption.DotNetCore/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using RockLib.Encryption;
+
+namespace Example.Encryption.DotNetCore
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var originalValue = "This is some string that we want to encrypt";
+
+            Console.WriteLine($"Original Value: {originalValue}");
+
+            var encryptedValue = Crypto.Encrypt(originalValue);
+
+            Console.WriteLine($"Encrypted Value: {encryptedValue}");
+
+            var decryptedValue = Crypto.Decrypt(encryptedValue);
+
+            Console.WriteLine($"Decrypted Value: {decryptedValue}");
+
+            Console.ReadLine();
+        }
+    }
+}
