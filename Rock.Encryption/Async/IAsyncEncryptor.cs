33ad9f0d47e4f9b40e0ce7da144583e00a5e95f0
Brian Friesen <brianfriesen@quickenloans.com>
2017-10-04 15:37:01 -0400

    Add asynchronous interfaces

---
+++ Rock.Encryption/Async/IAsyncEncryptor.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace RockLib.Encryption.Async
+{
+    /// <summary>
+    /// Defines methods for asynchronous encryption.
+    /// </summary>
+    public interface IAsyncEncryptor
+    {
+        /// <summary>
+        /// Asynchronously encrypts the specified plain text.
+        /// </summary>
+        /// <param name="plainText">The plain text.</param>
+        /// <returns>A task whose result represents the encrypted value as a string.</returns>
+        Task<string> EncryptAsync(string plainText);
+
+        /// <summary>
+        /// Asynchronously encrypts the specified plain text.
+        /// </summary>
+        /// <param name="plainText">The plain text.</param>
+        /// <returns>A task whose result represents the encrypted value as a byte array.</returns>
+        Task<byte[]> EncryptAsync(byte[] plainText);
+    }
+}
