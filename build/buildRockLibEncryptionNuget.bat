748c479e8fdee360fb39e1cc09a14a3d01c55b14
Justin Moss <Justin.Moss@gmail.com>
2017-11-20 14:17:39 -0500

    Update Version

--- build/buildRockLibEncryptionNuget.bat
+++ build/buildRockLibEncryptionNuget.bat
@@ -4,4 +4,4 @@ msbuild /p:Configuration=Release /t:Clean ..\Rock.Encryption\RockLib.Encryption.
 
 msbuild /p:Configuration=Release /t:Rebuild ..\Rock.Encryption\RockLib.Encryption.csproj
 
-msbuild /t:pack /p:PackageOutputPath=..\..\..\builtPackages  /p:Configuration=Release ..\Rock.Encryption\RockLib.Encryption.csproj
+msbuild /t:pack /p:PackageOutputPath=..\builtPackages  /p:Configuration=Release ..\Rock.Encryption\RockLib.Encryption.csproj
