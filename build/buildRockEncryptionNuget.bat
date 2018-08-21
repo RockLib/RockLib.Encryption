4ff052edbdc193cf53b3e701d7ca6c6aff27fc53
Brian Friesen <brianfriesen@quickenloans.com>
2018-08-21 15:29:27 -0400

    Remove build folder

--- build/buildRockEncryptionNuget.bat
+++
@@ -1,2 +0,0 @@
-msbuild /p:Configuration=Release ..\Rock.Encryption\Rock.Encryption.csproj
-nuget pack ..\Rock.Encryption\Rock.Encryption.csproj -Properties Configuration=Release
--- build/buildRockLibEncryptionNuget.bat
+++
@@ -1,7 +0,0 @@
-nuget restore -SolutionDirectory ../  ../Rock.Encryption\RockLib.Encryption.csproj
-
-msbuild /p:Configuration=Release /t:Clean ..\Rock.Encryption\RockLib.Encryption.csproj
-
-msbuild /p:Configuration=Release /t:Rebuild ..\Rock.Encryption\RockLib.Encryption.csproj
-
-msbuild /t:pack /p:PackageOutputPath=..\builtPackages  /p:Configuration=Release ..\Rock.Encryption\RockLib.Encryption.csproj
