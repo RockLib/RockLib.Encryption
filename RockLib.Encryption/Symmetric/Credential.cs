2597da3592a0f1016e57a7e51f81ae440c52e172
Brian Friesen <brianfriesen@quickenloans.com>
2019-01-09 14:34:35 -0500

    Allow the value of the key function to be cached

--- RockLib.Encryption/Symmetric/Credential.cs
+++ RockLib.Encryption/Symmetric/Credential.cs
@@ -25,16 +25,30 @@ namespace RockLib.Encryption.Symmetric
         /// <param name="algorithm">The <see cref="SymmetricAlgorithm"/> that will be used for a symmetric encryption or decryption operation.</param>
         /// <param name="ivSize">The size of the initialization vector that is used to add entropy to encryption or decryption operations.</param>
         /// <param name="name">The name of this credential.</param>
+        /// <param name="cacheKeyValue">Whether to cache the value of the <paramref name="key"/> function.</param>
         /// <param name="key">A function that returns the symmetric key to be returned by the <see cref="GetKey()"/> method.</param>
-        public Credential(Func<byte[]> key, SymmetricAlgorithm algorithm = DefaultAlgorithm, ushort ivSize = DefaultIVSize, string name = null)
+        public Credential(Func<byte[]> key, SymmetricAlgorithm algorithm = DefaultAlgorithm, ushort ivSize = DefaultIVSize, string name = null, bool cacheKeyValue = false)
         {
+            if (key == null) throw new ArgumentNullException(nameof(key));
             if (!Enum.IsDefined(typeof(SymmetricAlgorithm), algorithm)) throw new ArgumentOutOfRangeException(nameof(algorithm), $"{nameof(algorithm)} value is not defined: {algorithm}.");
             if (ivSize <= 0) throw new ArgumentOutOfRangeException(nameof(ivSize), $"{nameof(ivSize)} must be greater than 0.");
 
             Algorithm = algorithm;
             IVSize = ivSize;
             Name = name;
-            _key = key ?? throw new ArgumentNullException(nameof(key));
+
+            if (!cacheKeyValue)
+                _key = key;
+            else
+            {
+                var k = new Lazy<byte[]>(key);
+                _key = () =>
+                {
+                    var copy = new byte[k.Value.Length];
+                    k.Value.CopyTo(copy, 0);
+                    return copy;
+                };
+            }
         }
 
         /// <summary>
