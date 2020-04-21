7e2c6f0b5b7115fc4d47e6264a76845b3d98ab76
Justin Moss <justin.moss@gmail.com>
2020-04-21 12:49:17 -0400

    Improve main example and remove redundant examples

---
+++ Example.Symmetric/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RockLib.Encryption.Symmetric;
+using RockLib.Encryption.Symmetric.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace Example.DependencyInjection
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args);
+
+            Console.WriteLine("Select the service to run:");
+            Console.WriteLine($"1) {nameof(DependencyInjectionEncryptionService)}");
+            Console.WriteLine($"2) {nameof(StaticCryptoEncryptionService)}");
+
+            while (true)
+            {
+                switch (Console.ReadKey(false).KeyChar)
+                {
+                    case '1':
+                        return hostBuilder.ConfigureServices(services =>
+                        {
+                            // Configuring a symmetric crypto programmatically
+                            services.AddSymmetricCrypto()
+                                .AddCredential("Aes", "1J9Og/OaZKWdfdwM6jWMpvlr3q3o7r20xxFDN7TEj6s=")
+                                .AddCredential("DES", "2LQliivTtNo=", SymmetricAlgorithm.DES, 8)
+                                .AddCredential("RC2", "v6/CJsBIAK4rKs0hJR+JoA==", SymmetricAlgorithm.RC2, 8)
+                                .AddCredential("Rijndael", "CQSImVlbvJMZcnrkzT3/ouW1klt6STljrDjRiBzIsSk=", SymmetricAlgorithm.Rijndael)
+                                .AddCredential("TripleDES", "qHIqXosjnkzkUM9yp2aE+J+aey9w53+B", SymmetricAlgorithm.TripleDES, 8);
+
+                            services.AddHostedService<DependencyInjectionEncryptionService>();
+                        });
+                    case '2':
+                        return hostBuilder.ConfigureServices(services =>
+                        {
+                            // Symmetric crypto will be loaded and configured from configuration
+                            services.AddHostedService<StaticCryptoEncryptionService>();
+                        });
+                }
+            }
+        }
+    }
+}
